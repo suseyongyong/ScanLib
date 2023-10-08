a88b1fe42065a3f49be7365b8f721083a2888d50
roger <suseliyong@163.com>
2023-10-08 18:01:53 +0800

    Initial commit

---
+++ Example/ScanLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ScanLib
+//
+//  Created by roger on 10/08/2023.
+//  Copyright (c) 2023 roger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
